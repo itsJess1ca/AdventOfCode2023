c78dfdee60ff06d21289ac7980edee8d411ec3c6
itsJess1ca <jessica.ide247@gmail.com>
2023-12-01 17:42:20 +0000

    cleanup whitespace

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -2,8 +2,6 @@ import { stringByLine } from "../utils/string-by-line";
 import { parseLine } from "./parse-line";
 import { inputString } from "./__input_string";
 
-
-
 export function day1(input: string): number {
   return stringByLine(input)
     .map(parseLine)
